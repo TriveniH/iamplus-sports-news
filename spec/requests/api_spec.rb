ca40df8f3f30f92f327cebec12f3c9ca04cd385f
Meenal <89meenal.shandilya@gmail.com>
2017-01-12 18:19:48 +0530

    Covered all recap spec

--- spec/requests/api_spec.rb
+++ spec/requests/api_spec.rb
@@ -68,6 +68,7 @@ describe 'API Spec' do
 
   describe 'Previews' do
     context 'with valid event id' do
+      # Preview for EPL
       specify 'when no preview available for the event yet' do
         get '/sports_news', {sport: "EPL", event_id: "1643340"}
         expect( last_response.status ).to eq 404
@@ -81,6 +82,116 @@ describe 'API Spec' do
         expect( parsed_response[ :content ][ :paragraphs ]).not_to be_empty
         expect( parsed_response[ :content ][ :paragraphs ][0]).not_to be_nil
       end
+
+      # Preview for MLB
+      specify 'when no preview available for the event yet' do
+        get '/sports_news', {sport: "MLB", event_id: "1643340"}
+        expect( last_response.status ).to eq 404
+        expect( parsed_response[ :eventId ]).to be_nil
+      end
+      specify 'when preview available for the event' do
+        get '/sports_news', {sport: "MLB", event_id: "1677896"}
+        expect( last_response.status).to eq 200
+        expect( parsed_response[ :eventId ]).to eq "1677896"
+        expect( parsed_response[ :headline ]).not_to be_nil
+        expect( parsed_response[ :content ][ :paragraphs ]).not_to be_empty
+        expect( parsed_response[ :content ][ :paragraphs ][0]).not_to be_nil
+      end
+
+      # Preview for NBA
+      specify 'when no preview available for the event yet' do
+        get '/sports_news', {sport: "NBA", event_id: "1643340"}
+        expect( last_response.status ).to eq 404
+        expect( parsed_response[ :eventId ]).to be_nil
+      end
+      specify 'when preview available for the event' do
+        get '/sports_news', {sport: "NBA", event_id: "1674447"}
+        expect( last_response.status).to eq 200
+        expect( parsed_response[ :eventId ]).to eq "1674447"
+        expect( parsed_response[ :headline ]).not_to be_nil
+        expect( parsed_response[ :content ][ :paragraphs ]).not_to be_empty
+        expect( parsed_response[ :content ][ :paragraphs ][0]).not_to be_nil
+      end
+
+      # Preview for NFL
+      specify 'when no preview available for the event yet' do
+        get '/sports_news', {sport: "NFL", event_id: "1643340"}
+        expect( last_response.status ).to eq 404
+        expect( parsed_response[ :eventId ]).to be_nil
+      end
+      specify 'when preview available for the event' do
+        get '/sports_news', {sport: "NFL", event_id: "1635749"}
+        expect( last_response.status).to eq 200
+        expect( parsed_response[ :eventId ]).to eq "1635749"
+        expect( parsed_response[ :headline ]).not_to be_nil
+        expect( parsed_response[ :content ][ :paragraphs ]).not_to be_empty
+        expect( parsed_response[ :content ][ :paragraphs ][0]).not_to be_nil
+      end
+    end
+  end
+
+  describe 'recaps' do
+    context 'with valid event id' do
+    # recap for EPL
+      specify 'when no recap available for the event yet' do
+        get '/sports_news', {sport: "EPL", event_id: "1643340", event_status_id: "4"}
+        expect( last_response.status ).to eq 404
+        expect( parsed_response[ :eventId ]).to be_nil
+      end
+      specify 'when recap available for the event' do
+        get '/sports_news', {sport: "EPL", event_id: "1643241", event_status_id: "4"}
+        expect( last_response.status).to eq 200
+        expect( parsed_response[ :eventId ]).to eq "1643241"
+        expect( parsed_response[ :headline ]).not_to be_nil
+        expect( parsed_response[ :content ][ :paragraphs ]).not_to be_empty
+        expect( parsed_response[ :content ][ :paragraphs ][0]).not_to be_nil
+      end
+=begin
+    # recap for MLB
+      specify 'when no recap available for the event yet' do
+        get '/sports_news', {sport: "MLB", event_id: "1643340", event_status_id: "4"}
+        expect( last_response.status ).to eq 404
+        expect( parsed_response[ :eventId ]).to be_nil
+      end
+      specify 'when recap available for the event' do
+        get '/sports_news', {sport: "MLB", event_id: "1677891", event_status_id: "4"}
+        expect( last_response.status).to eq 200
+        expect( parsed_response[ :eventId ]).to eq "1677891"
+        expect( parsed_response[ :headline ]).not_to be_nil
+        expect( parsed_response[ :content ][ :paragraphs ]).not_to be_empty
+        expect( parsed_response[ :content ][ :paragraphs ][0]).not_to be_nil
+      end
+=end
+    # recap for NFL
+      specify 'when no recap available for the event yet' do
+        get '/sports_news', {sport: "NFL", event_id: "1643340", event_status_id: "4"}
+        expect( last_response.status ).to eq 404
+        expect( parsed_response[ :eventId ]).to be_nil
+      end
+      specify 'when recap available for the event' do
+        get '/sports_news', {sport: "NFL", event_id: "1635731", event_status_id: "4"}
+        expect( last_response.status).to eq 200
+        expect( parsed_response[ :eventId ]).to eq "1635731"
+        expect( parsed_response[ :headline ]).not_to be_nil
+        expect( parsed_response[ :content ][ :paragraphs ]).not_to be_empty
+        expect( parsed_response[ :content ][ :paragraphs ][0]).not_to be_nil
+      end
+
+    # recap for NBA
+      specify 'when no recap available for the event yet' do
+        get '/sports_news', {sport: "NBA", event_id: "1643340", event_status_id: "4"}
+        expect( last_response.status ).to eq 404
+        expect( parsed_response[ :eventId ]).to be_nil
+      end
+      specify 'when recap available for the event' do
+        get '/sports_news', {sport: "NBA", event_id: "1674447", event_status_id: "4"}
+        expect( last_response.status).to eq 200
+        expect( parsed_response[ :eventId ]).to eq "1674447"
+        expect( parsed_response[ :headline ]).not_to be_nil
+        expect( parsed_response[ :content ][ :paragraphs ]).not_to be_empty
+        expect( parsed_response[ :content ][ :paragraphs ][0]).not_to be_nil
+      end
+
     end
   end
 
