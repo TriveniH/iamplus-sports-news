f221ec268f8551ea80c237d38963e8aca1154795
Meenal <89meenal.shandilya@gmail.com>
2017-02-10 17:46:04 +0530

    Adding data base files.

---
+++ sports_direct_databases/directSavedHeadlines.rb
@@ -0,0 +1,15 @@
+class DirectSavedHeadLines
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :article_id,    type:String
+  field :publication_date, type:String
+  field :author,  type:String
+  field :source, type:String
+  field :title, type:String
+  field :synopsis, type:String
+  field :body, type:String
+  field :league_name, type:String
+
+  index({ created_at: 1 }, {expire_after_seconds: 86400})
+end
