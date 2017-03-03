6bf1a9bb23dc73411d1b9fefe16110b2b70fd551
Meenal <89meenal.shandilya@gmail.com>
2017-03-03 13:26:44 +0530

    changes fetch data to everyday

--- sports_direct/directDataFactory.rb
+++ sports_direct/directDataFactory.rb
@@ -24,12 +24,11 @@ module DirectDataFactory
 	NEWER_THAN_PARAM_KEY = "&newerThan="
 
 	def DirectDataFactory.fetch_all_data
-=begin
+
 		fetch_preview
 		sleep 5
 		fetch_recap
-		sleep 5 
-=end		
+		sleep 5 		
 		fetch_headline
 	end
 
